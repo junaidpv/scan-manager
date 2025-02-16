55a007480664381f8176563264154c72b3b4281b
Junaid PV <junu.pv@gmail.com>
2025-02-16 12:27:43 +0530

    Formatting

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,7 +7,11 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![commands::greet, commands::create_project, commands::get_projects])
+        .invoke_handler(tauri::generate_handler![
+            commands::greet,
+            commands::create_project,
+            commands::get_projects
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
