7852df3b63888a7f94e6e093b884b9fc25a58798
Junaid PV <junu.pv@gmail.com>
2025-02-16 11:52:22 +0530

    All commands to be registered together

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -114,9 +114,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
-        .invoke_handler(tauri::generate_handler![create_project])
-        .invoke_handler(tauri::generate_handler![get_projects])
+        .invoke_handler(tauri::generate_handler![greet, create_project, get_projects])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
