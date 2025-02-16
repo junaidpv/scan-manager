6a1d066adf0fb0f4a1fb93fac04db1d4df0de0ff
Junaid PV <junu.pv@gmail.com>
2025-02-16 12:36:41 +0530

    Store created and updated timestamps.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,9 +3,32 @@ interface ThumbnailItem {
     description: string;
 }
 
-// This represents the structure of the data that is stored in the projects.json file.
+/**
+ * Represents a project items
+ */
 interface ProjectItem {
+    /**
+     * The name of the project item.
+     */
     name: string;
+
+    /**
+     * The location where the item was scanned.
+     */
     scan_location: string;
+
+    /**
+     * A brief description of the project.
+     */
     description: string;
+
+    /**
+     * The timestamp when the project item was created.
+     */
+    created_at: Number;
+
+    /**
+     * The timestamp when the project item was last updated.
+     */
+    updated_at: Number;
 }
