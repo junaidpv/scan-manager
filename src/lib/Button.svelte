1e0d1da5011adb9720b3d300460ddda3c97aed79
Junaid PV <junu.pv@gmail.com>
2025-02-16 13:54:09 +0530

    Introuce Button component.

---
+++ src/lib/Button.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    let {title = 'Unkown', description = '', icon, button_color} = $props();
+
+    let button_color_calculated = $state();
+
+    $effect(() => {
+		if (button_color) {
+            button_color_calculated = 'text-' + button_color;
+        }
+	});
+</script>
+<button title={description}><div>{ title }</div>{#if icon }<i class="bi bi-{icon} fs-1 { button_color_calculated }"></i>{/if}</button>
--- src/lib/SidebarItem.svelte
+++ src/lib/SidebarItem.svelte
@@ -1,14 +1,7 @@
 <script>
+    import Button from "./Button.svelte";
     let {title = 'Unkown', description = '', icon, button_color} = $props();
-
-    let button_color_calculated = $state();
-
-    $effect(() => {
-		if (button_color) {
-            button_color_calculated = 'text-' + button_color;
-        }
-	});
 </script>
 <div class="card">
-    <button title={description}><div>{ title }</div>{#if icon }<i class="bi bi-{icon} fs-1 { button_color_calculated }"></i>{/if}</button>
+    <Button title={title} description={description} icon={icon} button_color={button_color}/>
 </div>
